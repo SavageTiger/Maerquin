b47a62e84eee21c0b5982782e91e00f344293e7f
Sven Hagemann <sven@savagetiger.org>
2018-02-25 13:47:41 +0100

    Symfony3 and Maerquin bundle skeleton

---
+++ README.md
@@ -0,0 +1,4 @@
+Maerquin
+========
+
+A Symfony project created on February 25, 2018, 1:24 pm.
---
+++ src/MaerquinBundle/MaerquinBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MaerquinBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MaerquinBundle extends Bundle
+{
+}
